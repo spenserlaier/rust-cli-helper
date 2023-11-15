a90a0e6356cba0571fa7f2361815e470fe585409
Spenser Laier <spenserlaier@gmail.com>
2023-11-14 22:24:52 -0500

    refactoring parsing functionality; new utility functions

--- src/argument_utils.rs
+++ src/argument_utils.rs
@@ -77,66 +77,95 @@ fn parse_arg_type(input_str: &str, stored_types: &HashMap<ArgName, ArgType>) -> 
         Err(String::from("unable to parse argument type"))
     }
 }
+fn parse_single_argument(arg_name: &str, arg_type: ArgType, arg_val: &str) -> Argument{
+    let parsed_arg_name = ArgName::ArgName(arg_name.to_string());
+    let parsed_arg_val = parse_arg_value(arg_val, arg_type);
+    match parsed_arg_val {
+        ArgVal::ArgValNone => {
+            Argument::SingleArgument(parsed_arg_name)
+        }
+        not_none_type => {
+            Argument::PairedArgument(parsed_arg_name, not_none_type)
+        }
+    }
+}
+fn remove_leading_dashes(arg: &str) -> &str {
+    // strip leading dashes
+    let mut idx = 0;
+    while arg.chars().nth(idx) == Some('-') {
+        idx += 1;
+    }
+    let stripped_arg = &arg[idx..];
+    if arg.len() == 0 {
+        panic!("invalid argument length. Arguments cannot contain only dashes")
+    }
+    stripped_arg
+}
+fn is_embedded_arg(arg: &str) -> bool {
+    if arg.contains("=") &&
+        arg.chars().filter(|&c| c == '=').count() == 1{
+        let mut components = arg.split("=");
+
+    }
+    false
+}
+fn parse_embedded_arg(arg: &str, stored_types: &HashMap<ArgName, ArgType>) -> Option<Argument> {
+    if arg.contains("=") {
+        if arg == "=" {
+            panic!("An inputted argument cannot contain an uncoupled equals sign");
+        }
+        if arg.chars().filter(|&c| c == '=').count() >= 2 {
+            panic!("An inputted argument cannot contain more than one equals sign");
+        }
+        else{
+            let mut parsed_arg_iter = arg.split("=");
+            let unparsed_arg_name = parsed_arg_iter.nth(0).unwrap().to_string();
+            let unparsed_arg_value = parsed_arg_iter.nth(1).unwrap().to_string();
+            let arg_type = parse_arg_type(&unparsed_arg_name, stored_types).unwrap();
+            let parsed_argument = parse_single_argument(&unparsed_arg_name, arg_type, &unparsed_arg_value);
+            return Some(parsed_argument);
+        }
+    }
+    None
+}
+fn parse_isolated_arg(arg: &str, arg_types: &HashMap<ArgName, ArgType>) -> Argument{
+    let parsed_arg_name = ArgName::ArgName(arg.to_string());
+    if arg_types.contains_key(&parsed_arg_name) {
+        Argument::SingleArgument(parsed_arg_name)
+    }
+    else{
+        panic!("unrecognized argument name");
+    }
+}
 pub fn parse_arguments(input: Vec<String>, arg_types: HashMap<ArgName, ArgType>) -> Vec<Argument> {
     let mut idx = 0;
     let mut parsed_arguments: Vec<Argument> = Vec::new();
     while idx < input.len() {
-        let arg_name = input.get(idx).unwrap();
-        if arg_name.chars().nth(0) == Some('-') && arg_name.chars().nth(idx) == Some('-') {
-            //remove leading dashes
-            idx += 1;
-        }
-        let parsed_arg_name: ArgName;
-        let parsed_arg_val: ArgVal;
-        if arg_name.contains("=") {
-            if arg_name.chars().filter(|&c| c == '=').count() >= 2 {
-                //TODO: since args are inputted by the user, should probably return an error
-                //instead of panicking
-                panic!("An inputted argument cannot contain more than one equals sign");
-            }
-            else{
-                let mut parsed_arg_iter = arg_name.split("=");
-                parsed_arg_name = ArgName::ArgName(parsed_arg_iter.nth(0).unwrap().to_string());
-            }
+        let mut increment = 1;
+        let current_arg_name = remove_leading_dashes(input.get(idx).unwrap());
+        if is_embedded_arg(current_arg_name) {
+            let embedded_arg = parse_embedded_arg(current_arg_name, &arg_types);
+            parsed_arguments.push(embedded_arg.unwrap());
         }
         else{
-            parsed_arg_name = ArgName::ArgName(arg_name.to_string());
+            if idx < input.len() -1 {
+                let parsed_arg_type = parse_arg_type(current_arg_name, &arg_types).unwrap();
+                let raw_arg_value = input.get(idx+1).unwrap();
+                let argument = parse_single_argument(current_arg_name, parsed_arg_type, raw_arg_value);
+                parsed_arguments.push(argument);
+                increment = 2;
+            }
+            else{
+                //panic!("A non-embedded argument expected a value but wasn't provided with one");
+                let parsed_isolated_arg = parse_isolated_arg(current_arg_name, &arg_types);
+                parsed_arguments.push(parsed_isolated_arg);
+            }
         }
-
-        let arg_type = parse_arg_type(arg_name, &arg_types).unwrap();
-        match arg_type {
-            ArgType::ArgTypeUsize => {
-                if let Some(arg_val_str) = input.get(idx+1) {
-                    let arg_val = parse_arg_value(arg_val_str, arg_type);
-                    let new_arg = Argument::PairedArgument(ArgName::ArgName(arg_name.clone()), arg_val);
-                    parsed_arguments.push(new_arg);
-                }
-                else{
-                    panic!("Missing required value for parameter {}", arg_name.to_string());
-                }
-                idx += 1;
-            },
-            ArgType::ArgTypeString => {
-                if let Some(arg_val_str) = input.get(idx+1) {
-                        let new_arg = Argument::PairedArgument(
-                            ArgName::ArgName(arg_name.to_string()),
-                            ArgVal::ArgValString(arg_val_str.to_string()));
-                        parsed_arguments.push(new_arg);
-                }
-                else{
-                    panic!("Missing required value for parameter {}", arg_name.to_string());
-                }
-                idx += 1;
-            },
-            ArgType::ArgTypeNoValue => {
-                let new_arg = Argument::SingleArgument(ArgName::ArgName(arg_name.to_string()));
-                parsed_arguments.push(new_arg);
-            },
-        }
-        idx += 1;
+        idx += increment;
     }
     parsed_arguments
 }
+
 #[cfg(test)]
 mod tests {
     use super::*;
